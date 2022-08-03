745d66337ef407dc8aa4c08adae8c4f71e8b39c3
Sol Ferraro <solagus0501@gmail.com>
2022-08-03 14:57:05 -0300

    agregando info

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+let botones = document.getElementById("botones")
+
+botones.innerHTML = `
+                    <div class="mx-5">
+                        <button class="btn btn-danger my-2 mx-1">Mis Intereses</button>
+                        <button class="btn btn-danger my-2 mx-1">Añadir Seccion</button>
+                        <button class="btn btn-danger my-2 mx-1">Mas</button>
+                    </div>`
